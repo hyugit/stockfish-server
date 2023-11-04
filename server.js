0a9eaf4d984842be935d98090d893c164728170a
Bigeard <robin.bigeard@gmail.com>
2023-11-04 20:57:57 +0000

    fix little bug and return only the best move

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 8989
 const stockfish = require("stockfish");
 const engine = stockfish();
-const fenregex = "/^([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw]\s(-|K?Q?k?q?)\s(-|[a-h][36])\s(0|[1-9][0-9]*)\s([1-9][0-9]*)/"
 
-engine.onmessage = function(msg) {
+engine.onmessage = (msg) => {
   console.log(msg);
 };
 
@@ -17,22 +15,15 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.post('/', (request, response) => {
-
-  if (!request.body.fen.match(fenregex)) {
-    response.send("Invalid fen string");
-    return;
-  }
-  
-// if chess engine replies
-  engine.onmessage = function(msg) {
-    console.log(msg);
+  // if chess engine replies
+  engine.onmessage = (msg) => {
     // in case the response has already been sent?
     if (response.headersSent) {
         return;
     }
     // only send response when it is a recommendation
     if (typeof(msg == "string") && msg.match("bestmove")) {
-        response.send(msg);
+        response.send(msg.substring(9, 13));
     }
   }
 
@@ -46,6 +37,5 @@ app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 })
